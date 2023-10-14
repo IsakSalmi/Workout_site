57db75d93352ba60c8e0ee77fa582b851c3e1eb3
isakSalmi <hansisak11@gmail.com>
2023-10-14 19:14:15 +0200

    added flask

---
+++ Proj/Srever.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, Response
+
+app = Flask(__name__, template_folder="Templates")
+
+
+@app.route('/', methods=['GET'])
+def index_get():
+    return None
+
+
+if __name__ == '__main__':
+    app.run(host='', port=80, debug=True)
