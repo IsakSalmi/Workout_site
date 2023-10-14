33a1ededc6ee1a4fad34b3777690ac4b0b37fed5
isakSalmi <hansisak11@gmail.com>
2023-10-14 22:18:10 +0200

    added a log in quarry

--- DB.py
+++ DB.py
@@ -1,13 +1,26 @@
 import sqlite3
 from flask import g
+from enum import Enum
+
+
+class ReturnType(Enum):
+    SUCCESS = True
+    FAIL = False
 
 
 DATABASE = "DB.sqlite"
-def get_db():
-    db = getattr(g, "_database", None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+db = sqlite3.connect(DATABASE)
+curser = db.cursor()
 
 
-def add_user(curser, username, password, renterpassword):
+def add_user(username, password, renterpassword):
+    curser.execute("SELECT * FROM user WHERE username = ?", ("test",))
+    answer = curser.fetchall()
+    print(answer)
+    if not answer and password == renterpassword:
+        curser.execute("INSERT INTO user(username, password) VALUES (?, ?)", ("test", "test"))
+        db.commit()
+        return ReturnType.SUCCESS
+    else:
+        return ReturnType.FAIL
+
